2bccecbb1dfbd56aea81d78b1dea33e0e6241317
Liam Seymour <liamgseymour@gmail.com>
2020-04-11 13:40:47 -0400

    Implement load command. Various fixes. More comments.

--- roster.c
+++ roster.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LINE 1024
+
 /* Helper functions */
 int compare(Student *s1, Student *s2);
 int compareNames(char *first1, char *last1, char *first2, char *last2);
@@ -20,6 +22,7 @@ void printPreOrder( Student *root );
 
 /* Command functions */
 int add(char *first, char* last, int points, int year, int house, Student *houses[]);
+int load(char *filepath, Student *houses[]);
 
 int main(int argc, char **argv)
 {
@@ -41,45 +44,51 @@ int main(int argc, char **argv)
 		// Command: inorder
 		if (strcmp(command, "inorder") == 0) 
 		{
+			printf("Inorder print-out of roster:\n");
 			// Print each house
 			for (int i = 0; i < HOUSES; ++i) 
 			{
-				printf("%s House\n\n\n", HOUSE_NAMES[i]);
+				printf("\n%s House\n\n", HOUSE_NAMES[i]);
 				printInOrder(houses[i]);
 			}
 			
 			// Print deceased students
-			printf("Deceased\n\n\n");
+			printf("\nDeceased\n\n");
 			printInOrder(houses[HOUSES]);
 		}	
+
 		// Command: preorder
 		else if (strcmp(command, "preorder") == 0) 
 		{
+			printf("Preorder print-out of roster:\n");
 			// Print each house
 			for (int i = 0; i < HOUSES; ++i) 
 			{
-				printf("%s House\n\n\n", HOUSE_NAMES[i]);
+				printf("\n%s House\n\n", HOUSE_NAMES[i]);
 				printPreOrder(houses[i]);
 			}
 			
 			// Print deceased students
-			printf("Deceased\n\n\n");
+			printf("\nDeceased\n\n");
 			printPreOrder(houses[HOUSES]);
 		}
+		
 		// Command: postorder
 		else if (strcmp(command, "postorder") == 0) 
 		{
+			printf("Postorder print-out of roster:\n");
 			// Print each house
 			for (int i = 0; i < HOUSES; ++i) 
 			{
-				printf("%s House\n\n\n", HOUSE_NAMES[i]);
+				printf("\n%s House\n\n", HOUSE_NAMES[i]);
 				printPostOrder(houses[i]);
 			}
 			
 			// Print deceased students
-			printf("Deceased\n\n\n");
+			printf("\nDeceased\n\n");
 			printPostOrder(houses[HOUSES]);
 		}
+		
 		// Command: add
 		else if (strcmp(command, "add") == 0) 
 		{
@@ -111,18 +120,35 @@ int main(int argc, char **argv)
 						first, last);
 			else { 
 				add(first, last, points, year, houseNumber, houses);
-				printf("Added %s %s to the roster.\n", first, last);
+				printf("Added %s %s to roster.\n", first, last);
 			}
 		} 
+
+		// Command: load
+		else if (strcmp(command, "load") == 0)
+		{
+			char filepath[2024];
+			sscanf(line, "load %s", filepath);
+			int error = load(filepath, houses); 
+			if (error != 0)
+				printf("Load failed.\n");
+				// TODO: Fix error codes
+			else
+				printf("Successfully loaded data from file %s.\n", filepath);
+		}
+
+		// Non-existent command
 		else if (strcmp(command, "quit") != 0) 
 		{
 			// Catch all other commands, not including quit.
 			printf("Unknown command: %s\n", command);
+			
+			// Fix command remaining unchanged when user hits enter with no command.
+			command[0] = '\0';
 		}
 	}
+
 	free(line);
-
-
 
 	return 0;
 }
@@ -187,10 +213,11 @@ void printStudent( Student *s )
 	char name[1024];
 	sprintf(name, "%s %s", s->first, s->last);
 
-	printf("%-25s Points: %d\tYear: %d\tHouse: %s\n", name, s->points, s->year, 
+	printf("%-24s Points: %d\tYear: %d\tHouse: %s\n", name, s->points, s->year, 
 			HOUSE_NAMES[s->house]);
 }
 
+/* Prints an in-order traversal of the Student tree root */
 void printInOrder( Student *root )
 {
 	if (root != NULL) 
@@ -201,6 +228,7 @@ void printInOrder( Student *root )
 	}
 } 
 
+/* Prints an post-order traversal of the Student tree root */
 void printPostOrder( Student *root )
 {
 	if (root != NULL) 
@@ -211,6 +239,7 @@ void printPostOrder( Student *root )
 	}
 }
 
+/* Prints an pre-order traversal of the Student tree root */
 void printPreOrder( Student *root )
 {
 	if (root != NULL) 
@@ -221,6 +250,8 @@ void printPreOrder( Student *root )
 	}
 }
 
+/* Creates and adds the given student to the roster of the given house. Does NOT check
+ * for valid data or if student is already present in house.  */
 int add(char *first, char* last, int points, int year, int house, Student *houses[])
 {
 	Student *s = malloc(sizeof(Student));
@@ -237,3 +268,58 @@ int add(char *first, char* last, int points, int year, int house, Student *house
 	 	
 	return 0;
 }
+
+
+/* Load in students from filepath. Return error code, or 0 on success. */
+int load(char *filepath, Student *houses[])
+{
+	FILE* in = fopen(filepath, "r");
+	if (in == NULL)
+		return 1;
+		// TODO: Fix error codes.
+	
+	char first[1024];
+	char last[1024];
+	int points;
+	int year;
+	char house[1024];
+
+	char line[MAX_LINE] = "";
+	while (fgets(line, MAX_LINE, in) != NULL) {
+		sscanf(line, "%s %s %d %d %s", first, last, &points, &year, house);
+
+		// Get corresponding house number, leave it at -1 if none match.
+		int houseNumber = -1;
+		for (int i = 0; i < HOUSES; ++i)
+			if (strcmp(HOUSE_NAMES[i], house) == 0)
+				houseNumber = i;
+
+		// Invalid house name
+		if (houseNumber == -1) 
+		{
+			printf("Invalid house name: %s\n", house);			
+			return 2;
+			// TODO: Fix error codes.
+		}
+		// Invalid year
+		else if (year > 7 || year < 1)
+		{
+			printf("Invalid year: %d\n", year);			
+			return 3;
+			// TODO: Fix error codes.
+		}
+		// Make sure that name is not in the roster already
+		else if (search(houses[houseNumber], first, last) != NULL) 
+		{
+			printf("Add failed. Student named %s %s already present in roster.\n", 
+					first, last);
+			return 4;
+			// TODO: Fix error codes.
+		}
+		else 
+			add(first, last, points, year, houseNumber, houses);
+	}
+
+	return 0;
+}
+
